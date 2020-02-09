cbf698633dfa0e3903775ecd612c979bc4ddfb9b
dane2k5500 <diework10@gmail.com>
2020-02-09 01:56:18 -0600

    Updates

---
+++ precache-manifest.4665d39712ecb4425f19b339ec7e3b23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfc4ea846732844973a2eb230b406588",
+    "url": "/ToDoListApp/index.html"
+  },
+  {
+    "revision": "b53a3948fedcb427c4cf",
+    "url": "/ToDoListApp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1702c35032113dd46378",
+    "url": "/ToDoListApp/static/js/2.7fc87db8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/ToDoListApp/static/js/2.7fc87db8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b53a3948fedcb427c4cf",
+    "url": "/ToDoListApp/static/js/main.9d227e21.chunk.js"
+  },
+  {
+    "revision": "f6330c488ad3aa77bd3b",
+    "url": "/ToDoListApp/static/js/runtime-main.7ba25474.js"
+  }
+]);
